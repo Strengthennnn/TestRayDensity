de2a0f617695d74ff04b36cd5222ac01d99aecf0
sth <1028839096@qq.com>
2020-04-19 22:28:54 +0800

    Initial TestRayDensity project.

---
+++ cmake-build-debug/CMakeFiles/TestRayDensity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestRayDensity.dir/main.cpp.o"
+  "TestRayDensity"
+  "TestRayDensity.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestRayDensity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
